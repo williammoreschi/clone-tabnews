378a83f07645797e2538ed9c8ccf4a92afa305dd
William Moreschi <williammoreschi@gmail.com>
2025-02-03 23:23:28 -0300

    adiciona scripts `lint:check`, `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-  return <h1>Buscando evoluir a cada dia, construindo um futuro melhor para nossa familia. ❤️😀</h1>
+function Home() {
+  return (
+    <h1>
+      Buscando evoluir a cada dia, construindo um futuro melhor para nossa
+      familia. ❤️😀
+    </h1>
+  );
 }
 
 export default Home;
