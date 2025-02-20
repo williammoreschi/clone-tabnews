a6757547134bee0275ad815b219fff277841a920
William Moreschi <williammoreschi@gmail.com>
2025-02-20 15:03:13 -0300

    Finalizando `api/v1/status` com testes de integração

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from 'infra/database.js';
+import { parse } from 'path';
 
 async function status(req, res) {
-  const result = await database.query('SELECT NOW();');
-  console.log(result.rows[0]);
-  return res.status(200).json({msg:'OK'});
+  const updatedAt = new Date().toISOString();
+  
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+  
+  const databaseMaxConnectionResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionValue = databaseMaxConnectionResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName]
+  });
+  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+
+  return res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnectionValue),
+        opened_connections: databaseOpenedConnectionsValue
+      }
+    },
+  });
 }
 
 export default status;
