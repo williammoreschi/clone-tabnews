8d5823b39e49dfb934c7c96fbbe15c484b5eb31d
William Moreschi <williammoreschi@gmail.com>
2025-02-26 21:12:42 -0300

    Melhora a robustez do npm test com `async-retry` e `orchestrator.js`

---
+++ tests/orchestrator.js
@@ -0,0 +1,30 @@
+import retry from 'async-retry';
+
+async function waitForAllServices(){
+  await webForWebServicer();
+
+  async function webForWebServicer() {
+    return retry(fetchStatusPage,{
+      retries: 100,
+      maxTimeout: 1000,
+      onRetry: (error, attempt) => {
+        console.log(`Attempt ${attempt} - Failed to fetch status page: ${error.message}`);
+      },
+    })
+    
+    async function fetchStatusPage() {
+      try {
+        const response = await fetch("http://localhost:3000/api/v1/status");
+        if (response.status !== 200) {
+          throw new Error(`HTTP error ${response.status}`);
+        }
+      } catch (error) {
+        throw error;
+      }
+     }
+  }
+}
+
+export default {
+  waitForAllServices
+}
