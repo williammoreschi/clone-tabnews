c89b2cd7f24e959cf101838f15794570c8b9b195
William Moreschi <williammoreschi@gmail.com>
2026-02-10 09:00:42 -0300

    refactor: extrai lógica de autenticação do controller de session para model
    Move a lógica de autenticação do controller de session para a model Authentication, deixando o
    controller mais simples e alinhado ao padrão de responsabilidades.

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -1,8 +1,6 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
-import user from "models/user.js";
-import password from "models/password.js";
-import { UnauthorizedError } from "infra/errors.js";
+import authentication from "models/authentication.js";
 
 const router = createRouter();
 
@@ -13,25 +11,10 @@ export default router.handler(controller.errorHandler);
 async function postHandler(req, res) {
   const userInputValue = req.body;
 
-  try {
-    const storedUser = await user.findOneByEmail(userInputValue.email);
+  const authenticateUser = await authentication.getAuthenticateUser(
+    userInputValue.email,
+    userInputValue.password,
+  );
 
-    const correctPasswordMatch = await password.compare(
-      userInputValue.password,
-      storedUser.password,
-    );
-
-    if (!correctPasswordMatch) {
-      throw new UnauthorizedError({
-        message: "Senha não confere.",
-        action: "Verifique se os dados estão corretos.",
-      });
-    }
-  } catch (error) {
-    throw new UnauthorizedError({
-      message: "Dados de autenticação não conferem.",
-      action: "Verifique se os dados estão corretos.",
-    });
-  }
   return res.status(201).json({});
 }
