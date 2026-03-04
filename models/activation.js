296abfa5b0fe78ca423fb8a1396f68d608ee1a5f
William Moreschi <williammoreschi@gmail.com>
2026-03-04 07:20:45 -0300

    feat: add `activation.findOneValidById()` and `orchestrator.extractUUID()`

--- models/activation.js
+++ models/activation.js
@@ -1,6 +1,7 @@
 import dedent from "dedent";
 import database from "infra/database";
 import email from "infra/email";
+import { NotFoundError } from "infra/errors";
 import webserver from "infra/scripts/webserver";
 
 const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000; // Expira em 15 minutos
@@ -27,11 +28,11 @@ async function create(userId) {
   }
 }
 
-async function findOneByUserId(userId) {
-  const user = await runSelectQuery(userId);
-  return user;
+async function findOneValidById(tokenId) {
+  const activationTokenObject = await runSelectQuery(tokenId);
+  return activationTokenObject;
 
-  async function runSelectQuery(userId) {
+  async function runSelectQuery(tokenId) {
     const results = await database.query({
       text: `
         SELECT
@@ -39,10 +40,20 @@ async function findOneByUserId(userId) {
         FROM
           user_activation_tokens
         WHERE
-          user_id = $1
+          id = $1 
+          AND used_at IS NULL
+          AND expires_at > NOW()
+        LIMIT 1
       `,
-      values: [userId],
+      values: [tokenId],
     });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "Token de ativação inválido ou expirado.",
+        action: "Faça um novo cadastro.",
+      });
+    }
 
     return results.rows[0];
   }
@@ -63,7 +74,7 @@ async function sendEmailToUser(user, activationToken) {
 const activation = {
   create,
   sendEmailToUser,
-  findOneByUserId,
+  findOneValidById,
   EXPIRATION_IN_MILLISECONDS,
 };
 
--- tests/integration/_use_cases/registration-flow.test.js
+++ tests/integration/_use_cases/registration-flow.test.js
@@ -1,3 +1,4 @@
+import webserver from "infra/scripts/webserver";
 import activation from "models/activation";
 import orchestrator from "tests/orchestrator.js";
 
@@ -41,15 +42,20 @@ describe("Use case: Registration Flow (all successful)", () => {
   test("Receive activation email", async () => {
     const lastEmail = await orchestrator.getLastEmail();
 
-    const activationToken = await activation.findOneByUserId(
-      createUserResponseBody.id,
-    );
-
     expect(lastEmail.sender).toBe("<contato@tabnews.com.br>");
     expect(lastEmail.recipients[0]).toBe("<registrationflowtest@gmail.com>");
     expect(lastEmail.subject).toBe("Ative sua conta no Clone Tabnews");
     expect(lastEmail.text).toContain("registrationFlowTest");
-    expect(lastEmail.text).toContain(activationToken.id);
+
+    const activationTokenId = orchestrator.extractUUID(lastEmail.text);
+    expect(lastEmail.text).toContain(
+      `${webserver.origin}/cadastro/ativar/${activationTokenId}`,
+    );
+
+    const activeTokenObject =
+      await activation.findOneValidById(activationTokenId);
+    expect(activeTokenObject.user_id).toBe(createUserResponseBody.id);
+    expect(activeTokenObject.used_at).toBe(null);
   });
 
   test("Activate account", async () => {});
