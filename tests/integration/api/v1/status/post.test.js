f9c7411914277dc0bf2fa55dda9ade50e12c040c
William Moreschi <williammoreschi@gmail.com>
2026-03-30 17:26:46 -0300

    test: replace "Retrieving" with "Running" in `POST /api/v1/migrations` test descriptions

--- tests/integration/api/v1/status/post.test.js
+++ tests/integration/api/v1/status/post.test.js
@@ -7,7 +7,7 @@ beforeAll(async () => {
 
 describe("POST /api/v1/status", () => {
   describe("Anonymous `user`", () => {
-    test("`Retrieving` current `system status`", async () => {
+    test("`Running` current `system status`", async () => {
       const response = await fetch(`${webserver.origin}/api/v1/status`, {
         method: "POST",
       });
